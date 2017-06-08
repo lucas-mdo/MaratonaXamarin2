e9798236593bf1070753605a762495dc3ecbb340
lucasmdo <luketeoli@hotmail.com>
2017-06-08 12:46:02 -0300

    Added User model. Installed Microsoft Azure Mobile Client nuget for all projects.

---
+++ src/MaratonaApp/MaratonaApp/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MaratonaApp.Model
+{
+    /// <summary>
+    /// Usuário
+    /// </summary>
+    public class User
+    {
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+    }
+}
