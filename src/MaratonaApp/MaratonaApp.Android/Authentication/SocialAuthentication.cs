af0595db22a03726a352238200201130be7163ab
lucasmdo <luketeoli@hotmail.com>
2017-06-08 13:14:55 -0300

    Removed unnecessary usings.

--- src/MaratonaApp/MaratonaApp.Android/Authentication/SocialAuthentication.cs
+++ src/MaratonaApp/MaratonaApp.Android/Authentication/SocialAuthentication.cs
@@ -1,21 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using MaratonaApp.Authentication;
 using MaratonaApp.Droid.Authentication;
 using MaratonaApp.Helpers;
 using Microsoft.WindowsAzure.MobileServices;
 using Xamarin.Forms;
 
-[assembly: Xamarin.Forms.Dependency(typeof(SocialAuthentication))]
+[assembly: Dependency(typeof(SocialAuthentication))]
 namespace MaratonaApp.Droid.Authentication
 {
     public class SocialAuthentication : IAuthentication
