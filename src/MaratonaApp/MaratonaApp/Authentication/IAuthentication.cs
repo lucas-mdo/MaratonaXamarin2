9a86e05fe8b56ff7599808f36889ed7d30178fbe
lucasmdo <luketeoli@hotmail.com>
2017-06-08 12:51:29 -0300

    Added interface for auth. Installed Xamarin Plugin Settings nuget for all projects.

---
+++ src/MaratonaApp/MaratonaApp/Authentication/IAuthentication.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace MaratonaApp.Authentication
+{
+    public interface IAuthentication
+    {
+        Task<MobileServiceUser> LoginAsync(MobileServiceClient client,
+            MobileServiceAuthenticationProvider provider,
+            IDictionary<string, string> parameters = null);
+    }
+}
