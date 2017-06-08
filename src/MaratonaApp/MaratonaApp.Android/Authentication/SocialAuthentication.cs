03004151783743b795440c3641ec8420defda87d
lucasmdo <luketeoli@hotmail.com>
2017-06-08 13:13:40 -0300

    Added Android auth using DependencyServices.

---
+++ src/MaratonaApp/MaratonaApp.Android/Authentication/SocialAuthentication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MaratonaApp.Authentication;
+using MaratonaApp.Droid.Authentication;
+using MaratonaApp.Helpers;
+using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SocialAuthentication))]
+namespace MaratonaApp.Droid.Authentication
+{
+    public class SocialAuthentication : IAuthentication
+    {
+        public async Task<MobileServiceUser> LoginAsync(MobileServiceClient client,
+            MobileServiceAuthenticationProvider provider, IDictionary<string, string> parameters = null)
+        {
+            try
+            {
+                //Get user
+                var user = await client.LoginAsync(Forms.Context, provider);
+
+                //Store user's information in Settings
+                Settings.AuthToken = user?.MobileServiceAuthenticationToken ?? string.Empty;
+                Settings.UserId = user?.UserId;
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                //TODO: Log error
+                throw;
+            }
+        }
+    }
+}
